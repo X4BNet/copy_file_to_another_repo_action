3c6c6c4ae0aacb3c13608178b518ed4261901aa8
Mathew <mat999@gmail.com>
2025-06-17 12:07:53 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,13 +21,35 @@ echo "Cloning destination git repository"
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 
-if [[ "$API_TOKEN_GITHUB" != "" ]]; then
-  git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
-else
-  echo "$SSH_KEY_GITHUB" > /tmp/ssh.key
-  chmod 0600 /tmp/ssh.key
-  export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i /tmp/ssh.key" 
-  git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "git@github.com:$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+
+rc=1
+if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
+then
+  if [[ "$API_TOKEN_GITHUB" != "" ]]; then
+    set +e
+    git clone --single-branch --branch $INPUT_DESTINATION_BRANCH_CREATE "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+    rc=$?
+    set -e
+  else
+    echo "$SSH_KEY_GITHUB" > /tmp/ssh.key
+    chmod 0600 /tmp/ssh.key
+    export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i /tmp/ssh.key" 
+    set +e
+    git clone --single-branch --branch $INPUT_DESTINATION_BRANCH_CREATE "git@github.com:$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+    rc=$?
+    set -e
+  fi
+fi
+
+if [[ "$rc" != "0" ]]; then
+  if [[ "$API_TOKEN_GITHUB" != "" ]]; then
+    git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+  else
+    echo "$SSH_KEY_GITHUB" > /tmp/ssh.key
+    chmod 0600 /tmp/ssh.key
+    export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i /tmp/ssh.key" 
+    git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "git@github.com:$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+  fi
 fi
 
 echo "Copying contents to git repo"
